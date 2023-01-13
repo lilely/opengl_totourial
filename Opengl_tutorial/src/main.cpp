49e8700beb15fb9f9284f32df87a1b0703322d48
jinxing <jinxing@edaijia-inc.cn>
2023-01-13 23:13:35 +0800

    init commit

---
+++ Opengl_tutorial/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Opengl_tutorial
+//
+//  Created by 金星 on 2023/1/13.
+//  Copyright © 2023 com.corp.jinxing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
