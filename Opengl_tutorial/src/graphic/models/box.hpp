f2cf6655268efa5ba265c8209f174eabb76b117f
lilely <lilely@163.com>
2023-02-08 23:40:21 +0800

    modification

--- Opengl_tutorial/src/graphic/models/box.hpp
+++ Opengl_tutorial/src/graphic/models/box.hpp
@@ -60,41 +60,43 @@ public:
         VAO.generate();
         VAO.bind();
         
-        VBO.generate();
-        VBO.bind();
-        VBO.setData<GLfloat>(static_cast<GLuint>(vertices.size() * 3), &vertices[0], GL_STATIC_DRAW);
-        VBO.clear();
+        VAO["VBO"] = BufferObject(GL_ARRAY_BUFFER);
+        VAO["VBO"].generate();
+        VAO["VBO"].bind();
+        VAO["VBO"].setData<GLfloat>(static_cast<GLuint>(vertices.size() * 3), &vertices[0], GL_STATIC_DRAW);
+        VAO["VBO"].clear();
         
-        offsetVBO.generate();
-        offsetVBO.bind();
-        offsetVBO.setData<glm::vec3>(BOX_UPPER_BOUND, NULL, GL_DYNAMIC_DRAW);
-        offsetVBO.clear();
+        VAO["offsetVBO"] = BufferObject(GL_ARRAY_BUFFER);
+        VAO["offsetVBO"].generate();
+        VAO["offsetVBO"].bind();
+        VAO["offsetVBO"].setData<glm::vec3>(BOX_UPPER_BOUND, NULL, GL_DYNAMIC_DRAW);
+        VAO["offsetVBO"].clear();
 
-        
-        sizeVBO.generate();
-        sizeVBO.bind();
-        sizeVBO.setData<glm::vec3>(BOX_UPPER_BOUND, NULL, GL_DYNAMIC_DRAW);
-        sizeVBO.clear();
+        VAO["sizeVBO"] = BufferObject(GL_ARRAY_BUFFER);
+        VAO["sizeVBO"].generate();
+        VAO["sizeVBO"].bind();
+        VAO["sizeVBO"].setData<glm::vec3>(BOX_UPPER_BOUND, NULL, GL_DYNAMIC_DRAW);
+        VAO["sizeVBO"].clear();
 
-        EBO = BufferObject(GL_ELEMENT_ARRAY_BUFFER);
-        EBO.generate();
-        EBO.bind();
-        EBO.setData<GLuint>(static_cast<GLuint>(indices.size()), &indices[0], GL_STATIC_DRAW);
+        VAO["EBO"] = BufferObject(GL_ELEMENT_ARRAY_BUFFER);
+        VAO["EBO"].generate();
+        VAO["EBO"].bind();
+        VAO["EBO"].setData<GLuint>(static_cast<GLuint>(indices.size()), &indices[0], GL_STATIC_DRAW);
         
         // position attribute
-        VBO.bind();
-        VBO.setAttPointer<float>(0, 3, GL_FLOAT, 3, 0);
-        VBO.clear();
+        VAO["VBO"].bind();
+        VAO["VBO"].setAttPointer<float>(0, 3, GL_FLOAT, 3, 0);
+        VAO["VBO"].clear();
         
         // position
-        offsetVBO.bind();
-        offsetVBO.setAttPointer<glm::vec3>(1, 3, GL_FLOAT, 1, 0, 1);
-        offsetVBO.clear();
+        VAO["offsetVBO"].bind();
+        VAO["offsetVBO"].setAttPointer<glm::vec3>(1, 3, GL_FLOAT, 1, 0, 1);
+        VAO["offsetVBO"].clear();
 
         // size
-        sizeVBO.bind();
-        sizeVBO.setAttPointer<glm::vec3>(2, 3, GL_FLOAT, 1, 0, 1);
-        sizeVBO.clear();
+        VAO["sizeVBO"].bind();
+        VAO["sizeVBO"].setAttPointer<glm::vec3>(2, 3, GL_FLOAT, 1, 0, 1);
+        VAO["sizeVBO"].clear();
 
         ArrayObject::clear();
     }
@@ -103,13 +105,13 @@ public:
         shader.setMat4("model", glm::mat4(1.0f));
         size_t size = std::min((int)offsetVecs.size(), BOX_UPPER_BOUND);
         if(size != 0) {
-            offsetVBO.bind();
-            offsetVBO.updateData<glm::vec3>(0, (unsigned int)size, &offsetVecs[0]);
+            VAO["offsetVBO"].bind();
+            VAO["offsetVBO"].updateData<glm::vec3>(0, (unsigned int)size, &offsetVecs[0]);
 
-            sizeVBO.bind();
-            sizeVBO.updateData<glm::vec3>(0, (unsigned int)size, &sizeVecs[0]);
+            VAO["sizeVBO"].bind();
+            VAO["sizeVBO"].updateData<glm::vec3>(0, (unsigned int)size, &sizeVecs[0]);
             
-            sizeVBO.clear();
+            VAO["sizeVBO"].clear();
         }
         VAO.bind();
         VAO.draw(GL_LINES, static_cast<int>(indices.size()), GL_UNSIGNED_INT, 0, (GLsizei)size);
@@ -123,16 +125,10 @@ public:
     
     void cleanup() {
         VAO.cleanup();
-        VBO.cleanup();
-        EBO.cleanup();
-        offsetVBO.cleanup();
-        sizeVBO.cleanup();
     }
     
 private:
     ArrayObject VAO;
-    BufferObject VBO, offsetVBO, sizeVBO;
-    BufferObject EBO;
     
     std::vector<float> vertices;
     std::vector<unsigned int> indices;
